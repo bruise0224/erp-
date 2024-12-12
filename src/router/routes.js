e8e7f1897f4149dafe447216104ce784c6605378
bruise0224 <1985438967@qq.com>
2024-12-12 13:59:39 +0800

    Add files via upload

---
+++ src/router/routes.js
@@ -0,0 +1,114 @@
+const routers = [
+    {
+        path: '/',
+        component: () => import('@/views/Login.vue'),
+    },
+    {
+        name: 'login',
+        path: '/Login',
+        component: () => import('@/views/Login.vue')
+    },
+    {
+        name: '404',
+        path: '/:catchAll(.*)*',
+        component: () => import('@/views/404.vue')
+    },
+    {
+        name: 'layout',
+        path: '/Layout',
+        component: () => import('@/views/Layout/index.vue'),
+        children: [
+            {
+                name: '首页',
+                path: '/Welcome',
+                component: () => import('@/views/Welcome/index.vue'),
+            },
+            //##路由-文件夹##
+            {
+                path:'/COLLECT_INFO',
+                component:()=>import('@/views/COLLECT_INFO/index.vue'),
+                children:[
+                    //COLLECT_INFO##路由-文件##
+                    {
+                        path:'COLLECT_INFO',
+                        name:'送签社管理',
+                        component:()=> import('@/views/COLLECT_INFO/COLLECT_INFO.vue')
+                    },
+                ]
+            },
+            
+            {
+                path: '/DISTRIBUTION_INFO',
+                component: () => import('@/views/DISTRIBUTION_INFO/index.vue'),
+                children: [
+                    //DISTRIBUTION_INFO##路由-文件##
+                    {
+                        path: 'DISTRIBUTION_INFO',
+                        name: '3D分布',
+                        component: () => import('@/views/DISTRIBUTION_INFO/DISTRIBUTION_INFO.vue')
+                    },
+                ]
+            },
+
+            {
+                path: '/VISIT_INFO',
+                component: () => import('@/views/VISIT_INFO/index.vue'),
+                children: [
+                    //VISIT_INFO##路由-文件##
+                    {
+                        path: 'VISIT_INFO',
+                        name: '酒店添加',
+                        component: () => import('@/views/VISIT_INFO/VISIT_INFO.vue')
+                    },
+                ]
+            },
+
+            {
+                path: '/BORROW_INFO',
+                component: () => import('@/views/BORROW_INFO/index.vue'),
+                children: [
+                    //BORROW_INFO##路由-文件##                    
+                    {
+                        path:'PRE_BORROW_INFO',
+                        name:'借阅意向清单',
+                        component:()=> import('@/views/BORROW_INFO/PRE_BORROW_INFO.vue')
+                    },
+                                        
+                    {
+                        path: 'BORROW_INFO',
+                        name: '酒店信息',
+                        component: () => import('@/views/BORROW_INFO/BORROW_INFO.vue')
+                    },
+                ]
+            },
+
+            {
+                path: '/BOOK_INFO',
+                component: () => import('@/views/BOOK_INFO/index.vue'),
+                children: [
+                    //BOOK_INFO##路由-文件##
+                    {
+                        path: 'BOOK_INFO',
+                        name: '旅行团信息',
+                        component: () => import('@/views/BOOK_INFO/BOOK_INFO.vue')
+                    },
+                ]
+            },
+            {
+                name: '用户管理模块',
+                path: '/USER_INFO',
+                component: () => import('@/views/USER_INFO/index.vue'),
+                children: [
+                    //USER_INFO##路由-文件##
+                    {
+                        name: '用户管理',
+                        path: 'USER_INFO',
+                        component: () => import('@/views/USER_INFO/USER_INFO.vue')
+                    },
+                ]
+            },
+        ]
+    },
+]
+
+export default routers
---
+++ src/views/Welcome/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <el-carousel :interval="4000" type="card" height="250px">
+    <el-carousel-item v-for="item in 3" :key="item">
+      <el-image :src="require('@/assets/img/' + item + '.jpg')" fit="fit" />
+      <!-- <h3 text="2xl" justify="center">{{ item }}</h3> -->
+    </el-carousel-item>
+  </el-carousel>
+  <div class="search">
+    <el-form id="form1" class="form-part" :inline="true" :model="formData1">
+      <el-form-item label="团名" label-width="70">
+        <el-input id="book_name" v-model="formData1.book_name" placeholder="模糊查询" MaxLength="20" clearable></el-input>
+      </el-form-item>
+      <el-form-item label="团号" label-width="70">
+        <el-input id="author" v-model="formData1.author" placeholder="模糊查询" MaxLength="20" clearable></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button id="btn_SearchID1" type="primary" @click="onGoto()">
+          <el-icon>
+            <Search />
+          </el-icon>
+          搜索
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive, onMounted } from "vue";
+import { Search } from "@element-plus/icons-vue";
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+const formData1 = reactive({
+  book_name: '',
+  author: '',
+})
+
+const onGoto=()=>{
+  router.push({ path: '/BOOK_INFO/BOOK_INFO', query: { book_name: formData1.book_name, author:formData1.author } })
+}
+
+</script>
+
+<style scoped>
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+
+.search {
+  margin-top: 15px;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: aqua;
+}
+</style>
